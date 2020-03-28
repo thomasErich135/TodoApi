1d369e5dc189c205dcd65ccbf3c5ff867d07a066
Marcelo Martins Oliveira <31354681+mamartins1997@users.noreply.github.com>
2020-03-27 21:28:55 -0300

    Initial commit

---
+++ Todo.Domain/Commands/Contracts/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Flunt.Validations;
+
+namespace Todo.Domain.Commands.Contracts
+{
+    public interface ICommand : IValidatable
+    {
+    }
+}
---
+++ Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Todo.Domain.Entities
+{
+    public abstract class Entity : IEquatable<Entity>
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+
+        public bool Equals([AllowNull] Entity other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
